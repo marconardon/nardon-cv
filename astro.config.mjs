4f84f014f7b9a979a92289517c40884fcea8fa2e
Marco Nardon <62355029+marconardon@users.noreply.github.com>
2026-06-30 23:39:59 +0200

    added wrangler.jsonc to configure Cloudflare Workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   vite: {
     ssr: {
       external: ['svgo'],
     },
   },
+
+  adapter: cloudflare(),
 });
